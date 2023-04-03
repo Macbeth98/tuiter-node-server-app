07c3070f65fd2b54657fd373ae8de824b5333248
Mani <manichandra.teja@gmail.com>
2023-04-02 20:26:57 -0400

    added start script

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ tuitsController(app);
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`Server listening on port ${port}!`);
 });
