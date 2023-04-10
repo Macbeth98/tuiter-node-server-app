500fad2d26d3812f7cb1b81d5781d3b0b7574ad2
Mani <manichandra.teja@gmail.com>
2023-04-09 23:01:53 -0400

    schema updated

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import tuitsController from './controllers/tuits/tuits-controller.js';
 
 const CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+
 mongoose.connect(CONNECTION_STRING).then(() => {
   console.log('Connected to MongoDB!');
 });
--- tuits/tuits-schema.js
+++ tuits/tuits-schema.js
@@ -4,6 +4,13 @@ const schema = mongoose.Schema(
     tuit: String,
     likes: Number,
     liked: Boolean,
+    handle: String,
+    image: String,
+    replies: Number,
+    retuits: Number,
+    time: String,
+    topic: String,
+    username: String,
   },
   { collection: 'tuits' }
 );
